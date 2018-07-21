5044cd99d64f74d4d0a260a1f3f65639cb61b74a
khodani-M <khodanimufamadi@gmail.com>
2018-07-21 20:45:48 +0200

    Work on testing policy

--- Code/NFCBusinessCardLocal2/app/build/generated/source/r/debug/org/opencv/R.java
+++ Code/NFCBusinessCardLocal2/app/build/generated/source/r/debug/org/opencv/R.java
@@ -12,9 +12,9 @@ public final class R {
         public static final int show_fps = 0x7f0200fc;
     }
     public static final class id {
-        public static final int any = 0x7f070020;
-        public static final int back = 0x7f07002b;
-        public static final int front = 0x7f070065;
+        public static final int any = 0x7f070021;
+        public static final int back = 0x7f07002c;
+        public static final int front = 0x7f070066;
     }
     public static final class styleable {
         public static final int[] CameraBridgeViewBase = { 0x7f02004c, 0x7f0200fc };
