43b4089f8bc5a031a5c6ae648986f98a27396d90
khodani-M <khodanimufamadi@gmail.com>
2018-08-21 11:36:24 +0200

    updated splash screen

--- Code/NFCBusinessCardLocal2/app/src/main/java/com/example/www/nfcbusinesscardlocal/Splash.java
+++ Code/NFCBusinessCardLocal2/app/src/main/java/com/example/www/nfcbusinesscardlocal/Splash.java
@@ -1,7 +1,11 @@
 package com.example.www.nfcbusinesscardlocal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView imageView =findViewById(R.id.imageV);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+        imageView.startAnimation(animation);
+
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+
+                    Intent intent= new Intent(getApplicationContext(),LogIn.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        timer.start();
     }
 }
