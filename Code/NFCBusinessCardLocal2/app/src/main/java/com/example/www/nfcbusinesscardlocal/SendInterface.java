82b3d4ca24ac6e25fa7769c122d5e1d837482123
khodani-M <khodanimufamadi@gmail.com>
2018-09-06 16:14:05 +0200

    updated

---
+++ Code/NFCBusinessCardLocal2/app/src/main/java/com/example/www/nfcbusinesscardlocal/SendInterface.java
@@ -0,0 +1,84 @@
+package com.example.www.nfcbusinesscardlocal;
+
+import android.app.DialogFragment;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.signature.StringSignature;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class SendInterface extends DialogFragment {
+
+    private Button btn_nfc;
+    private Button btn_qr;
+    private Listener mListener;
+    public static final String TAG = SendInterface.class.getSimpleName();
+    public static SendInterface newInstance() {
+
+        return new SendInterface();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_send_interface, container, false);
+        btn_nfc = (Button) view.findViewById(R.id.btn_nfc);
+        btn_nfc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC))
+                {
+                    Toast.makeText(view.getContext(), "NFC is not available. Use QR option.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Intent mov = new Intent(view.getContext(), NFCActivity.class);
+                startActivity(mov);
+            }
+        });
+        btn_qr = (Button) view.findViewById(R.id.btn_qr);
+        btn_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mov = new Intent(view.getContext(), GenerateQRCode.class);
+                startActivity(mov);
+            }
+        });
+        return view;
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mListener = (MainActivity)context;
+        mListener.onDialogDisplayed();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener.onDialogDismissed();
+    }
+}
---
+++ Code/NFCBusinessCardLocal2/app/src/main/java/com/example/www/nfcbusinesscardlocal/TestUser.java
@@ -0,0 +1,191 @@
+package com.example.www.nfcbusinesscardlocal;
+
+import android.os.Parcelable;
+import android.telecom.Call;
+
+import junit.framework.Test;
+
+import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.security.MessageDigest;
+
+public class TestUser implements Serializable{
+    //Attributes
+    private String fullname, jobTitle, password;
+    private String companyName,mobileNumber,workTelephone,emailAddress,workAddress;
+    private String recievedCards="",imageUrl="";
+    //String facebook_link,linkedIn_link,googleplus_link;
+    public TestUser(){
+    }
+    public TestUser(TestUser copy)
+    {
+        fullname=copy.getFullname(); jobTitle=copy.getJobTitle();password=copy.getPassword();
+        companyName=copy.getCompanyName();mobileNumber=copy.getMobileNumber();workTelephone=copy.getWorkTelephone();
+        emailAddress=copy.getEmailAddress();workAddress=copy.getWorkAddress();
+        recievedCards=getRecievedCards();
+    }
+    //setters
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setPassword(String password) //throws NoSuchAlgorithmException,NoSuchProviderException
+    {
+        //byte[] salt=getSalt();
+        this.password = password;//getSecurePassword(password,salt);
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setWorkTelephone(String workTelephone) {
+        this.workTelephone = workTelephone;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setMobileNumber(String mobileNumber) { this.mobileNumber = mobileNumber; }
+
+    public void setWorkAddress(String workAddress) {this.workAddress = workAddress;}
+
+    public void setRecievedCards(String recievedCards){
+        this.recievedCards=recievedCards;
+    }
+
+    public void setImageUrl(String imageUrl){this.imageUrl=imageUrl;}
+    //getters
+
+    public String getFullname() {
+
+        if(fullname.isEmpty()||fullname.compareTo("")==0)
+        { return "n/a";}
+            return fullname;
+    }
+
+    public String getJobTitle() {
+        if((jobTitle.isEmpty())||jobTitle.compareTo("")==0)
+        { return "n/a";}
+        return jobTitle;
+    }
+
+    public String getPassword() {
+        if(password.isEmpty()||password.compareTo("")==0)
+        { return "n/a";}
+        return password;
+    }
+
+    public String getCompanyName() {
+        if(companyName.isEmpty()||companyName.compareTo("")==0)
+        { return "n/a";}
+        return companyName;
+    }
+
+    public String getWorkTelephone() {
+        if(workTelephone.isEmpty()||workTelephone.compareTo("")==0)
+        { return "n/a";}
+        return workTelephone;
+    }
+
+    public String getMobileNumber() {
+        if(mobileNumber.isEmpty()||mobileNumber.compareTo("")==0)
+        { return "n/a";}
+        return mobileNumber;}
+
+    public String getEmailAddress() {
+        if(emailAddress.isEmpty()||emailAddress.compareTo("")==0)
+        { return "n/a";}
+        return emailAddress;
+    }
+
+    public String getWorkAddress() {
+       if(workAddress.isEmpty()||workAddress.compareTo("")==0)
+        { return "n/a";}
+        return workAddress;
+    }
+    public String getRecievedCards(){
+        if(recievedCards.isEmpty()||recievedCards.compareTo("")==0||recievedCards.compareTo("[]")==0)
+            return "";
+        return recievedCards;
+    }
+    public String getImageUrl(){
+        if(imageUrl.isEmpty()||imageUrl.compareTo("")==0)
+            return "";
+        return imageUrl;
+    }
+    //class functions
+    public String getDetails()
+    {
+        String returnString="";
+            returnString+=getFullname()+"\n";
+            returnString+=getJobTitle()+"\n";
+            returnString+=getCompanyName()+"\n";
+            returnString+=getEmailAddress()+"\n";
+            returnString+=getMobileNumber()+"\n";
+            returnString+=getWorkTelephone()+"\n";
+            returnString+=getWorkAddress()+" \n";
+            returnString+=getImageUrl();
+
+        return returnString;
+    }
+    public String generateDetails()
+    {
+        String returnString="";
+            returnString+=getFullname()+"%";
+            returnString+=getJobTitle()+"%";
+            returnString+=getCompanyName()+"%";
+            returnString+=getEmailAddress()+"%";
+            returnString+=getMobileNumber()+"%";
+            returnString+=getWorkTelephone()+"%";
+            returnString+=getWorkAddress()+"%";
+            returnString+=getImageUrl();
+
+        return returnString;
+    }
+    private static String getSecurePassword(String passwordToHash, byte[] salt)
+    {
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(salt);
+            //Get the hash's bytes
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    //Add salt
+    private static byte[] getSalt() throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        //Always use a SecureRandom generator
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        //Create array for salt
+        byte[] salt = new byte[16];
+        //Get a random salt
+        sr.nextBytes(salt);
+        //return salt
+        return salt;
+    }
+}
